cb4586a88b26a488f714adfa3d54c2f3caf80bef
Matthew Foyle <matt@moltin.com>
2017-06-19 13:24:51 +0100

    add basic redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import MoltinApp from './reducers'
-
-let store = createStore(MoltinApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,13 +1,26 @@
-const cart = (state = [], action) => {
-  switch (action.type) {
-    case 'INCREMENT_CART':
-    return {
-      ...state,
-      cart: 1
-    };
-    default:
-      return state
-  }
+const initialState = {
+  fetching: false,
+  fetched: false,
+  orders: null,
+  error: null
 }
 
-export default cart
+const ProductsReducer = (state=initialState, action) => {
+  switch (action.type) {
+    case "Fetch_Products_Start": {
+      return {...state, fetching: true};
+    }
+    case "Fetch_Products_End": {
+      return {...state,
+         fetching: false,
+         fetched: true,
+         orders: action.payload
+       };
+    }
+    default: {
+      return {...state, fetching: false, error: action.payload};
+    }
+  }
+};
+
+export default ProductsReducer;
