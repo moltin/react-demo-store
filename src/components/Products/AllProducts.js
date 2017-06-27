a08f5f456580a8b5a493cdd3f821d53a2076eec0
Matthew Foyle <matt@moltin.com>
2017-06-27 18:54:37 +0100

    add dynamic overlay to all products page

--- src/components/Products/AllProducts.js
+++ src/components/Products/AllProducts.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import MailingList from '../global/MailingList';
 import ProductImage from './ProductImage';
+
+
+function mapStateToProps(state) {
+    return(state)
+}
 
 class AllProducts extends Component {
 
   render() {
-    var products = this.props.products.products
+
+    if(this.props.css !== null && this.props.products.products !== null) {
+      var products = this.props.products.products;
+      console.log(this.props)
+      var OverlayIsHidden = this.props.css.OverlayIsHidden;
+
+      var ChangeHidden = (event) => {
+        if(event === "unhide") {
+
+          this.props.dispatch((dispatch) => {
+            dispatch({type: "Unhide_Overlay"})
+          })
+        }
+        if(event === "hide") {
+
+          this.props.dispatch((dispatch) => {
+            dispatch({type: "hide_Overlay"})
+          })
+        }
+        else {
+          //console.log("something is fucked")
+        }
+      };
+    }
 
     return (
     <main role="main" id="container" className="main-container push">
@@ -16,15 +45,15 @@ class AllProducts extends Component {
               {products.data.map(function(product) {
                 var background = product.background_colour;
                 return (
-                  <a className="product-item" href={"/product/" + product.id} key={product.id}>
+                  <a className="product-item" href={"/product/" + product.id} key={product.id} onMouseOver={() => {ChangeHidden("unhide")}} onMouseLeave={() => {ChangeHidden("hide")}}>
                     <div className="product-image" style={{"background": background}}>
                         <ProductImage product={product} products={products}/>
                     </div>
-                    <div className="overlay hidden" aria-hidden="true">
+                    <div className={`overlay ${OverlayIsHidden}`} aria-hidden="true">
                         <div className="overlay-background" style={{"background": "#aaaaaa"}}></div>
                         <div className="overlay-content">
-                            <div className="title">Crown</div>
-                            <div className="price">$475</div>
+                            <div className="title">{product.name}</div>
+                            <div className="price">{'$' + product.meta.display_price.with_tax.amount/100}</div>
                         </div>
                     </div>
                   </a>
@@ -43,4 +72,4 @@ class AllProducts extends Component {
 }
 
 
-export default AllProducts;
+export default connect(mapStateToProps)(AllProducts);
