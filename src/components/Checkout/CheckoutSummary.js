2b35417a7de208d0133b25e514feb0f120c11c93
Matthew Foyle <matt@moltin.com>
2017-07-03 12:13:17 +0100

    revert changes to checkout

--- src/components/Checkout/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CheckoutItems from './CheckoutItems';
+import Loading from '../global/Loading';
+var api = require('../../utils/moltin.js');
 
 function mapStateToProps(state) {
     return(state)
@@ -8,7 +10,47 @@ function mapStateToProps(state) {
 
 class CheckoutSummary extends Component {
 
+  // a react lifecycle event, read more at http://busypeoples.github.io/post/react-component-lifecycle/
+  componentDidMount() {
+
+    // check if we already have a moltin products in the store
+    if(this.props.products.fetched === false) {
+
+      // dispatch an action to our redux reducers
+      this.props.dispatch((dispatch) => {
+
+          // this action will set a fetching field to true
+          dispatch({type: "Fetch_Products_Start"})
+
+          // get the moltin products from the API
+          api.GetProducts()
+
+          .then((products) => {
+            /* now that we have the products, this action will set fetching to false and fetched to true,
+            as well as add the moltin products to the store */
+            dispatch({type: "Fetch_Products_End", payload: products})
+          })
+      })
+    }
+
+    if(this.props.cart.fetched === false) {
+
+      this.props.dispatch((dispatch) => {
+
+        dispatch({type: "Fetch_Cart_Start"})
+
+        api.GetCartItems()
+
+        .then((cart) => {
+          dispatch({type: "Fetch_Cart_End", payload: cart})
+        })
+    })
+    }
+  }
+
   render() {
+
+      if(this.props.cart.fetched === true && this.props.products.fetched === true) {
 
         var tax = this.props.cart.cart.meta.display_price.with_tax.amount - this.props.cart.cart.meta.display_price.without_tax.amount;
 
@@ -30,8 +72,14 @@ class CheckoutSummary extends Component {
               </div>
           </div>
         )
+      }
 
-    }
+      return (
+          <div className="checkout-summary">
+            <Loading />
+          </div>
+      )
+      }
 }
 
 
